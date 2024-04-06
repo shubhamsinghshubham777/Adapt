9547b18432c510ab50be95b21ccbddc1fc29a821
Shubham Singh <shubhamsinghshubham777@gmail.com>
2024-04-06 22:17:41 +0530

    Rename IOSButtonStyles to match Apple docs & reduce redundancy in IOSButtonColors

--- adapt/src/commonMain/kotlin/design/adapt/ios/Button.kt
+++ adapt/src/commonMain/kotlin/design/adapt/ios/Button.kt
@@ -54,8 +54,13 @@ fun IOSButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     size: IOSButtonSize = IOSButtonSize.Small,
-    style: IOSButtonStyle = IOSButtonStyle.Borderless,
-    colors: IOSButtonColors = IOSButtonDefaults.buttonColors(),
+    style: IOSButtonStyle = IOSButtonStyle.Plain,
+    colors: IOSButtonColors = when (style) {
+        IOSButtonStyle.Plain -> IOSButtonDefaults.plainButtonColors()
+        IOSButtonStyle.Gray -> IOSButtonDefaults.grayButtonColors()
+        IOSButtonStyle.Tinted -> IOSButtonDefaults.tintedButtonColors()
+        IOSButtonStyle.Filled -> IOSButtonDefaults.filledButtonColors()
+    },
     icon: (@Composable () -> Unit)? = null,
     text: (@Composable () -> Unit)? = null,
     shape: Shape = IOSButtonDefaults.shape(
@@ -76,7 +81,7 @@ fun IOSButton(
     val onPressAlpha by animateFloatAsState(
         targetValue = if (isPressed) 0.5f else 0f,
         animationSpec = when {
-            style == IOSButtonStyle.Borderless && isPressed -> tween(durationMillis = 0)
+            style == IOSButtonStyle.Plain && isPressed -> tween(durationMillis = 0)
             else -> spring()
         },
     )
@@ -105,64 +110,18 @@ fun IOSButton(
         }
     }
 
-    val containerColor: Color = remember(style, enabled, isInLightMode, onMaterial, colors) {
-        when (style) {
-            IOSButtonStyle.Borderless -> {
-                if (enabled) colors.borderlessContainerColor
-                else colors.disabledBorderlessContainerColor
-            }
-
-            IOSButtonStyle.BezeledGray -> {
-                if (enabled) colors.bezeledGrayContainerColor
-                // The material color should only be used with light mode
-                else if (isInLightMode && onMaterial) colors.disabledOnMaterialBezeledGrayContainerColor
-                else colors.disabledBezeledGrayContainerColor
-            }
-
-            IOSButtonStyle.Bezeled -> {
-                if (enabled) colors.bezeledContainerColor
-                // The material color should only be used with light mode
-                else if (isInLightMode && onMaterial) colors.disabledOnMaterialBezeledContainerColor
-                else colors.disabledBezeledContainerColor
-            }
-
-            IOSButtonStyle.Filled -> {
-                if (enabled) colors.filledContainerColor
-                // The material color should only be used with light mode
-                else if (isInLightMode && onMaterial) colors.disabledOnMaterialFilledContainerColor
-                else colors.disabledFilledContainerColor
-            }
-        }
+    val containerColor: Color = remember(enabled, isInLightMode, onMaterial, colors) {
+        if (enabled) colors.containerColor
+        // The material color should only be used with light mode
+        else if (isInLightMode && onMaterial) colors.disabledOnMaterialTintedContainerColor
+        else colors.disabledContainerColor
     }
 
-    val contentColor: Color = remember(style, enabled, isInLightMode, onMaterial, colors) {
-        when (style) {
-            IOSButtonStyle.Borderless -> {
-                if (enabled) colors.borderlessContentColor
-                else colors.disabledBorderlessContentColor
-            }
-
-            IOSButtonStyle.BezeledGray -> {
-                if (enabled) colors.bezeledGrayContentColor
-                // The material color should only be used with light mode
-                else if (isInLightMode && onMaterial) colors.disabledOnMaterialBezeledGrayContentColor
-                else colors.disabledBezeledGrayContentColor
-            }
-
-            IOSButtonStyle.Bezeled -> {
-                if (enabled) colors.bezeledContentColor
-                // The material color should only be used with light mode
-                else if (isInLightMode && onMaterial) colors.disabledOnMaterialBezeledContentColor
-                else colors.disabledBezeledContentColor
-            }
-
-            IOSButtonStyle.Filled -> {
-                if (enabled) colors.filledContentColor
-                // The material color should only be used with light mode
-                else if (isInLightMode && onMaterial) colors.disabledOnMaterialFilledContentColor
-                else colors.disabledFilledContentColor
-            }
-        }
+    val contentColor: Color = remember(enabled, isInLightMode, onMaterial, colors) {
+        if (enabled) colors.contentColor
+        // The material color should only be used with light mode
+        else if (isInLightMode && onMaterial) colors.disabledOnMaterialTintedContentColor
+        else colors.disabledContentColor
     }
 
     Row(
@@ -174,7 +133,7 @@ fun IOSButton(
                 onClick = onClick,
             )
             .graphicsLayer {
-                alpha = if (style == IOSButtonStyle.Borderless) 1 - onPressAlpha else 1f
+                alpha = if (style == IOSButtonStyle.Plain) 1 - onPressAlpha else 1f
             }
             .background(color = containerColor, shape = shape)
             .drawWithCache {
@@ -183,7 +142,7 @@ fun IOSButton(
                     drawOutline(
                         outline = shape.createOutline(this.size, layoutDirection, density),
                         color = Color.Black.copy(alpha = 0.5f),
-                        alpha = if (style != IOSButtonStyle.Borderless) onPressAlpha else 0f,
+                        alpha = if (style != IOSButtonStyle.Plain) onPressAlpha else 0f,
                     )
                 }
             }
@@ -220,78 +179,81 @@ fun IOSButton(
 
 @Immutable
 data class IOSButtonColors(
-    val borderlessContainerColor: Color,
-    val borderlessContentColor: Color,
-    val bezeledGrayContainerColor: Color,
-    val bezeledGrayContentColor: Color,
-    val bezeledContainerColor: Color,
-    val bezeledContentColor: Color,
-    val filledContainerColor: Color,
-    val filledContentColor: Color,
-    val disabledBorderlessContainerColor: Color,
-    val disabledBorderlessContentColor: Color,
-    val disabledBezeledGrayContainerColor: Color,
-    val disabledBezeledGrayContentColor: Color,
-    val disabledBezeledContainerColor: Color,
-    val disabledBezeledContentColor: Color,
-    val disabledFilledContainerColor: Color,
-    val disabledFilledContentColor: Color,
-    val disabledOnMaterialBezeledGrayContainerColor: Color,
-    val disabledOnMaterialBezeledGrayContentColor: Color,
-    val disabledOnMaterialBezeledContainerColor: Color,
-    val disabledOnMaterialBezeledContentColor: Color,
-    val disabledOnMaterialFilledContainerColor: Color,
-    val disabledOnMaterialFilledContentColor: Color,
+    val containerColor: Color,
+    val contentColor: Color,
+    val disabledContainerColor: Color,
+    val disabledContentColor: Color,
+    val disabledOnMaterialTintedContainerColor: Color,
+    val disabledOnMaterialTintedContentColor: Color,
 )
 
 object IOSButtonDefaults {
     @Composable
-    fun buttonColors(
-        borderlessContainerColor: Color = Color.Transparent,
-        borderlessContentColor: Color = IOSTheme.colorScheme.systemBlue,
-        bezeledGrayContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
-        bezeledGrayContentColor: Color = IOSTheme.colorScheme.systemBlue,
-        bezeledContainerColor: Color = IOSTheme.colorScheme.systemBlue.copy(alpha = 0.15f),
-        bezeledContentColor: Color = IOSTheme.colorScheme.systemBlue,
-        filledContainerColor: Color = IOSTheme.colorScheme.systemBlue,
-        filledContentColor: Color = IOSTheme.colorScheme.systemWhite,
-        disabledBorderlessContainerColor: Color = Color.Transparent,
-        disabledBorderlessContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-        disabledBezeledGrayContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
-        disabledBezeledGrayContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-        disabledBezeledContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
-        disabledBezeledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-        disabledFilledContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
-        disabledFilledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-        disabledOnMaterialBezeledGrayContainerColor: Color = IOSTheme.colorScheme.systemWhite.copy(alpha = 0.12f),
-        disabledOnMaterialBezeledGrayContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-        disabledOnMaterialBezeledContainerColor: Color = IOSTheme.colorScheme.systemWhite.copy(alpha = 0.12f),
-        disabledOnMaterialBezeledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-        disabledOnMaterialFilledContainerColor: Color = IOSTheme.colorScheme.systemWhite.copy(alpha = 0.12f),
-        disabledOnMaterialFilledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
-    ): IOSButtonColors = IOSButtonColors(
-        borderlessContainerColor = borderlessContainerColor,
-        borderlessContentColor = borderlessContentColor,
-        bezeledGrayContainerColor = bezeledGrayContainerColor,
-        bezeledGrayContentColor = bezeledGrayContentColor,
-        bezeledContainerColor = bezeledContainerColor,
-        bezeledContentColor = bezeledContentColor,
-        filledContainerColor = filledContainerColor,
-        filledContentColor = filledContentColor,
-        disabledBorderlessContainerColor = disabledBorderlessContainerColor,
-        disabledBorderlessContentColor = disabledBorderlessContentColor,
-        disabledBezeledGrayContainerColor = disabledBezeledGrayContainerColor,
-        disabledBezeledGrayContentColor = disabledBezeledGrayContentColor,
-        disabledBezeledContainerColor = disabledBezeledContainerColor,
-        disabledBezeledContentColor = disabledBezeledContentColor,
-        disabledFilledContainerColor = disabledFilledContainerColor,
-        disabledFilledContentColor = disabledFilledContentColor,
-        disabledOnMaterialBezeledGrayContainerColor = disabledOnMaterialBezeledGrayContainerColor,
-        disabledOnMaterialBezeledGrayContentColor = disabledOnMaterialBezeledGrayContentColor,
-        disabledOnMaterialBezeledContainerColor = disabledOnMaterialBezeledContainerColor,
-        disabledOnMaterialBezeledContentColor = disabledOnMaterialBezeledContentColor,
-        disabledOnMaterialFilledContainerColor = disabledOnMaterialFilledContainerColor,
-        disabledOnMaterialFilledContentColor = disabledOnMaterialFilledContentColor,
+    fun plainButtonColors(
+        containerColor: Color = Color.Transparent,
+        contentColor: Color = IOSTheme.colorScheme.systemBlue,
+        disabledContainerColor: Color = Color.Transparent,
+        disabledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+        disabledOnMaterialTintedContainerColor: Color = Color.Transparent,
+        disabledOnMaterialTintedContentColor: Color = Color.Transparent,
+    ) = IOSButtonColors(
+        containerColor = containerColor,
+        contentColor = contentColor,
+        disabledContainerColor = disabledContainerColor,
+        disabledContentColor = disabledContentColor,
+        disabledOnMaterialTintedContainerColor = disabledOnMaterialTintedContainerColor,
+        disabledOnMaterialTintedContentColor = disabledOnMaterialTintedContentColor,
+    )
+
+    @Composable
+    fun grayButtonColors(
+        containerColor: Color = IOSTheme.colorScheme.fillTertiary,
+        contentColor: Color = IOSTheme.colorScheme.systemBlue,
+        disabledContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
+        disabledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+        disabledOnMaterialTintedContainerColor: Color = IOSTheme.colorScheme.systemWhite.copy(alpha = 0.12f),
+        disabledOnMaterialTintedContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+    ) = IOSButtonColors(
+        containerColor = containerColor,
+        contentColor = contentColor,
+        disabledContainerColor = disabledContainerColor,
+        disabledContentColor = disabledContentColor,
+        disabledOnMaterialTintedContainerColor = disabledOnMaterialTintedContainerColor,
+        disabledOnMaterialTintedContentColor = disabledOnMaterialTintedContentColor,
+    )
+
+    @Composable
+    fun tintedButtonColors(
+        containerColor: Color = IOSTheme.colorScheme.systemBlue.copy(alpha = 0.15f),
+        contentColor: Color = IOSTheme.colorScheme.systemBlue,
+        disabledContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
+        disabledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+        disabledOnMaterialTintedContainerColor: Color = IOSTheme.colorScheme.systemWhite.copy(alpha = 0.12f),
+        disabledOnMaterialTintedContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+    ) = IOSButtonColors(
+        containerColor = containerColor,
+        contentColor = contentColor,
+        disabledContainerColor = disabledContainerColor,
+        disabledContentColor = disabledContentColor,
+        disabledOnMaterialTintedContainerColor = disabledOnMaterialTintedContainerColor,
+        disabledOnMaterialTintedContentColor = disabledOnMaterialTintedContentColor,
+    )
+
+    @Composable
+    fun filledButtonColors(
+        containerColor: Color = IOSTheme.colorScheme.systemBlue,
+        contentColor: Color = IOSTheme.colorScheme.systemWhite,
+        disabledContainerColor: Color = IOSTheme.colorScheme.fillTertiary,
+        disabledContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+        disabledOnMaterialTintedContainerColor: Color = IOSTheme.colorScheme.systemWhite.copy(alpha = 0.12f),
+        disabledOnMaterialTintedContentColor: Color = IOSTheme.colorScheme.labelTertiary,
+    ) = IOSButtonColors(
+        containerColor = containerColor,
+        contentColor = contentColor,
+        disabledContainerColor = disabledContainerColor,
+        disabledContentColor = disabledContentColor,
+        disabledOnMaterialTintedContainerColor = disabledOnMaterialTintedContainerColor,
+        disabledOnMaterialTintedContentColor = disabledOnMaterialTintedContentColor,
     )
 
     fun shape(isIconOnly: Boolean, size: IOSButtonSize): Shape = RoundedCornerShape(
@@ -315,4 +277,4 @@ object IOSButtonDefaults {
 }
 
 enum class IOSButtonSize { Small, Medium, Large }
-enum class IOSButtonStyle { Borderless, BezeledGray, Bezeled, Filled }
+enum class IOSButtonStyle { Plain, Gray, Tinted, Filled }
